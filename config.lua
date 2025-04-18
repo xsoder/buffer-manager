3cf10a9038518a42c54d1039ebae5daa6b01094c
csodelinux <csodelinux@gmail.com>
2025-04-18 11:00:18 -0400

    fzf fix

---
+++ config.lua
@@ -0,0 +1,12 @@
+return {
+  options = {
+    mappings = {
+      open = "<Leader>bb",
+      vertical = "<Leader>bv",
+      horizontal = "<Leader>bs",
+    },
+    search = {
+      keybinding = "<Leader>/",
+    },
+  }
+}
